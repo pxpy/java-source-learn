3bc23005a913cc689cc7c579ca96aa70fd6f8b7a
panpan <panxin@panxin.me>
2023-04-27 18:45:39 +0800

    init

---
+++ practice/src/me/panxin/TestHshMap.java
@@ -0,0 +1,15 @@
+package me.panxin;
+
+import java.util.HashMap;
+
+/**
+ * @author PanXin
+ * @version $ Id: TestHshMap, v 0.1 2023/04/27 16:48 PanXin Exp $
+ */
+public class TestHshMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("p","x");
+        System.out.println(hashMap);
+    }
+}
